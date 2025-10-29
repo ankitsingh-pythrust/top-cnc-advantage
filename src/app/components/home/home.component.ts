45bd1541c0f520ea969d34daee0afee9f56c3bff
ankitsingh-pythrust <162984477+ankitsingh-pythrust@users.noreply.github.com>
2025-10-29 17:04:06 +0530

    Add/update src/app/components/home/home.component.ts

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component ({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, RouterModule, MatButtonModule, MatDividerModule, MatIconModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  public scrollTo(id: string): void {
+    const el = document.getElementById(id);
+    if (el) el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+}
